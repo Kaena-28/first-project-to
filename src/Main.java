d04b073133ee899d1832381208752f6f00811c46
Kaena-28 <kanykejdzanybekova0@gmail.com>
2024-12-09 12:28:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Базарбай");
+        System.out.println("        ойногону");
+        System.out.println("                кетти");
+
+        System.out.println("**********");
+        System.out.println("    ||");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("    ||");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println(" ||    ||");
+    }
+}
